b76dba3b68377f62c828727c7151c89b50d4798e
Enric <enrikk49@gmail.com>
2020-12-04 16:07:37 +0100

    Create tables recipe, ingredients and association between them

--- Cookbook/recipe/models.py
+++ Cookbook/recipe/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    ingredients = models.ManyToManyField(Ingredient, through='IngredientsRecipe')
+
+    def __str__(self):
+        return self.name
+
+class IngredientsRecipe(models.Model):
+    quantity = models.FloatField()
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
